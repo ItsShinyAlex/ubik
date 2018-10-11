03a6cac43a58eafb5d0daf2d01136b2683389200
Lyngall <soporte@esbrillante.com>
2018-10-11 04:53:44 -0500

    desktop maquetado full
    falta responsive y ajustes en el WPbakery

--- footer/footer-default.php
+++ footer/footer-default.php
@@ -15,7 +15,7 @@
                   CONTÁCTANOS
                 </h2>
                 <h3 class="padless">Comentanos que deseas y te ayudaremos.</h3>
-        <div class="flex">
+        <div class="flex" id="cflex">
            
             <div class="col c-col">
              
@@ -33,78 +33,98 @@
 
                 <div class="flex icons-bot">
                     <div class="col icon fcentered ">
-                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/whatsapp.png" alt="WhatsApp">
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/waicon.svg" alt="WhatsApp">
                     </div>
-                    <div class="col txt ">
+                    <div class="col txt v-center ">
+                            <p class="white phone">
+
+                                   <a class="white" href="https://wa.me/77721500518">777-21500518</a>
+
+
+                            </p>
+                    </div>
+                </div>
+                
+                   <div class="flex icons-bot">
+                    <div class="col icon fcentered ">
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/telicon.svg" alt="Telèfono">
+                    </div>
+                    <div class="col txt v-center">
                         <p class="white phone">
-                            <a class="white" href="https://wa.me/525519587101">5519587101</a><br>
+                            <a class="white" href="tel:77721500518">777-21500518 - Oficina</a><br>
+                            <a class="white" href="tel:7771847922">777-1847922 - Celular</a>
                         </p>
                     </div>
                 </div>
                
                 <div class="flex icons-bot">
                     <div class="col icon fcentered ">
-                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/phone.png" alt="teléfono">
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/clockicon.svg" alt="Horario">
                     </div>
-                    <div class="col txt">
+                    <div class="col txt v-center">
                         <p class="white phone">
-                            <a class="white" href="tel:5513282907">01 (55) 1328 2907</a>
-                        </p>
+                            Lunes a Viernes de 9:00 AM a 6:00 PM
+                         </p>
                     </div>
+                </div>
+                
+                
+                <div class="flex icons-bot">
+                    <div class="col icon fcentered">
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/locateicon.svg" alt="ubicación">
+                    </div>
+                    <div class="col txt v-center">
+                        <p>Dirección de la oficina</p>
+                    </div>
+                </div>
+                
+                <div class="fcentered mar-bot">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d940.7823156044791!2d-99.16812294669649!3d19.406820165643005!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d1ff154ab64aa3%3A0xe5ddf2cd58d48680!2sTuxpan+8%2C+Roma+Sur%2C+06760+Ciudad+de+M%C3%A9xico%2C+CDMX!5e0!3m2!1ses-419!2smx!4v1536244758591" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
                 </div>
                 
                 <div class="flex icons-bot">
                     <div class="col icon fcentered">
-                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/clock.png" alt="horario">
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/mailicon.svg" alt="email">
                     </div>
-                    <div class="col txt fcentered">
-                        <p >Lunes a Viernes de 9 a.m. 6 p.m. 
-                        Sábado de 8 a.m. a  3 p.m.</p>
-                    </div>
-                </div>
-                
-                <div class="flex icons-bot">
-                    <div class="col icon fcentered">
-                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/ubicacion.png" alt="ubicación">
-                    </div>
-                    <div class="col txt fcentered">
-                        <p>Torre Médica Tuxpan Núm. 8, Col Roma Sur, CDMX, Piso 2
-                        C.P 06760 
-                        Estamos a 40 metros del Metro Chilpancingo</p>
+                    <div class="col txt v-center">
+                       <a href="mailto:contacto@ubikbienesraices.com?subject=Contacto%20UBIK"><p>Contacto@ubikbienesraices.com</p></a> 
                     </div>
                 </div>
                 
-                <div class="flex mar-bot" id="followus">
-                    <div class="col txt fcentered" id="siguenos">
-                       <a href="https://business.facebook.com/Body-Balance-2160952823945786/" target="_blank"><p><b>SÍGUENOS</b></p></a>
+                <div class="flex mar-bot fcentered" id="followus">
+                    <div class="col txt v-center">
+                       <p>Síguenos en:</p>
                         
                     </div>
                     
                     <div class="col icon fcentered">
-                       <a href="https://business.facebook.com/Body-Balance-2160952823945786/" target="_blank"> <img src="../img/twittericon.svg" alt="Twitter"></a>
+                       <a href="https://twitter.com/" target="_blank"> <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/twittericon.svg" alt="Twitter"></a>
+                    </div>
                     
                     <div class="col icon fcentered">
-                       <a href="https://business.facebook.com/Body-Balance-2160952823945786/" target="_blank"> <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/face.png" alt="Facebook"></a>
+                       <a href="https://business.facebook.com/" target="_blank"> <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/facebookicon.svg" alt="Facebook"></a>
                        
                     </div>
                     <div class="col icon fcentered">
-                       <a href="https://www.instagram.com/bodybalance.mx/?hl=es-la" target="_blank"> <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/instagram-logo.png" alt="Facebook"></a>
+                       <a href="https://www.instagram.com" target="_blank"> <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/instagramicon.svg" alt="Instagram"></a>
                        
                     </div>
                 </div>
                 
-                <div class="mar-bot pad-left">
-                    <a href="https://goo.gl/maps/GapxsnfqX7y" class="btn-saqua" target="_blank">¿Cómo llegar?</a>    
-                </div>
                 
                 
-                <div class="fcentered">
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d940.7823156044791!2d-99.16812294669649!3d19.406820165643005!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d1ff154ab64aa3%3A0xe5ddf2cd58d48680!2sTuxpan+8%2C+Roma+Sur%2C+06760+Ciudad+de+M%C3%A9xico%2C+CDMX!5e0!3m2!1ses-419!2smx!4v1536244758591" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
-                </div>
+        
             </div>
             
             
             
+        </div>
+        
+        <div class="flex">
+            <div class="col"><p>© 2018 Todos los derechos reservados</p> </div>
+            <div class="col"> <a href="">
+                <p class="center">Soluciones Web: EsBrillante</p>
+            </a> </div>
         </div>
         
                        
